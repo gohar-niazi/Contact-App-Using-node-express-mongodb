e49034ed6d070f8ffac19eb7a501cf75d80a5e74
gohar-niazi <goharniazi.dev@gmial.com>
2025-10-17 19:08:27 +0500

    add Security

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,16 +1,31 @@
 import contacts from "../models/contact.js";
+import mongoose from "mongoose";
 
+// Home PAge Route
 export const getcontacts = async (req, res) => {
   const contact = await contacts.find();
   res.render("home", { contact });
 };
+
+// Single Contact with id show route
 export const getcontact = async (req, res) => {
-  const contact = await contacts.findById(req.params.id);
-  res.render("show-contact", { contact });
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.render("404", { message: "Invalid Id" });
+  }
+  try {
+    const contact = await contacts.findById(req.params.id);
+    if (!contact) return res.render("404", { message: "Contact Not Found" });
+    return res.render("show-contact", { contact });
+  } catch (error) {
+    return res.render("500", { message: error });
+  }
 };
+
+// Add contactpage Route
 export const addcontactpage = (req, res) => {
   res.render("add-contact", { error: null });
 };
+// Add contact dataroute
 export const addcontact = async (req, res) => {
   const { first_name, last_name, email, phone, address } = req.body;
   // Check if any field is empty
@@ -18,18 +33,53 @@ export const addcontact = async (req, res) => {
     // Send back the same form page with an error message
     return res.render("add-contact", { error: "All fields are required!" });
   }
-  const contact = await contacts.create(req.body);
-  res.redirect("/");
+
+  try {
+    const contact = await contacts.create(req.body);
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("500", { message: error });
+  }
 };
+
+// Update Contact with ID Route Page
 export const updatecontactpage = async (req, res) => {
-  const contact = await contacts.findById(req.params.id);
-  res.render("update-contact", { contact });
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.render("404", { message: "Invalid Id" });
+  }
+  try {
+    const contact = await contacts.findById(req.params.id);
+    if (!contact) return res.render("404", { message: "Contact Not Found" });
+    return res.render("update-contact", { contact });
+  } catch (error) {
+    return res.render("500", { message: error });
+  }
 };
+
+// Update Contact with ID Route
 export const updatecontact = async (req, res) => {
-  await contacts.findByIdAndUpdate(req.params.id, req.body);
-  res.redirect("/");
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.render("404", { message: "Invalid Id" });
+  }
+  try {
+    const contact = await contacts.findByIdAndUpdate(req.params.id, req.body);
+    if (!contact) return res.render("404", { message: "Contact Not Found" });
+    res.redirect("/");
+  } catch (error) {
+    return res.render("500", { message: error });
+  }
 };
+
+// Delte Contact Route
 export const deletecontact = async (req, res) => {
-  await contacts.findByIdAndDelete(req.params.id);
-  res.redirect("/");
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.render("404", { message: "Invalid Id" });
+  }
+  try {
+    const contact = await contacts.findByIdAndDelete(req.params.id);
+    if (!contact) return res.render("404", { message: "Contact Not Found" });
+    res.redirect("/");
+  } catch (error) {
+    return res.render("500", { message: error });
+  }
 };
